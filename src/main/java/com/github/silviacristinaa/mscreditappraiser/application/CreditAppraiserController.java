daa071bf527f8a14eee74690e48fa37ae67c1602
silviacristinaa <silviacristinaalexandre1@gmail.com>
2023-03-21 11:38:34 -0300

    primeiro commit

---
+++ src/main/java/com/github/silviacristinaa/mscreditappraiser/application/CreditAppraiserController.java
@@ -0,0 +1,15 @@
+package com.github.silviacristinaa.mscreditappraiser.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("credit-appraiser")
+public class CreditAppraiserController {
+
+    @GetMapping
+    public String status(){
+        return "ok";
+    }
+}
